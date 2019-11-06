4bee8ee96d8f88da9a4e074c2461201e264aee1a
Nash Hymet <nashhymet@gmail.com>
2019-11-06 17:44:24 -0600

    Finished Week 04, see the notes taken for more info

--- MyProgress/First Year/Semester 1/Python for Everyone/Week04/2_2_notes.py
+++ MyProgress/First Year/Semester 1/Python for Everyone/Week04/2_2_notes.py
@@ -1,3 +1,6 @@
+print("Output:")
+print("----------------------------------")
+print("\n")
 ### Expressions Part 2
 ## Numeric Expressions
 # ----------------------------------------------
@@ -46,4 +49,88 @@
 # print(x)
 # ----------------------------------------------
 
-##
+## Type Matters
+# Python knows what "type" everything is
+
+# Some operations are prohibited
+
+# You cannot "add 1" to a string
+
+# We can ask Python what type something is by using the
+# type() function
+
+# Example 1 - Outputs: hellothere
+#eee = 'hello' + 'there'
+#print(eee)
+
+# Example 2 - Will produce a Traceback error
+#eee = 'hello' + 'there'
+#eee = eee + 1
+#print(eee)
+# ----------------------------------------------
+
+## Several Types of Numbers
+# Numbers have two main types
+# - Integers are whole numbers:
+#   -14, -2, 0, 1, 100, 401233
+
+# - Floating Point Numbers have decimal parts:
+#   -2.5, 0.0, 98.6, 14.0
+
+# There are other number types - they are variations
+# on float and integer
+# ----------------------------------------------
+
+## Type Conversions
+# When you put an integer and floating point in an Expression
+# the integer is implicitly converted to a float
+
+# You can control this with the built-in functions int() and float()
+# Example 3 - Outputs: 199.0
+#print(float(99) + 100)
+#i = 42 # this gets outputted as 42.0
+#type(i)
+#f = float(i) # using this line
+#print(f)
+#type(f)
+# ----------------------------------------------
+
+## String Conversions
+# You can also use int() and float() to convert
+# between strings and integers
+
+# You will get an error if the string does not contain
+# numeric characters
+
+# Example 5 - Outputs a Traceback error
+#sval = '123'
+#type (sval)
+#print(sval + 1)
+
+# Example 6 - Outputs: 124
+#sval = '123'
+#type (sval)
+#ival = int(sval)
+#type(ival)
+#print(ival + 1)
+
+# Example 7 - Outputs a Traceback error
+#nsv = 'hello bob'
+#niv = int(nsv) # requires an int
+# ----------------------------------------------
+
+## User Input
+# We can instruct Python to pause and read data from
+# the user using the input() function
+
+# The input() function returns a string
+
+# Example 8 - Asks you who you are and prints a welcome message
+#nam = input('Who are you?')
+#print('Welcome', nam)
+# ----------------------------------------------
+
+
+
+print("----------------------------------\n")
+print("The end of the line: nothing is here")
---
+++ MyProgress/First Year/Semester 1/Python for Everyone/Week04/2_3_notes.py
@@ -0,0 +1,42 @@
+print("Output:")
+print("----------------------------------")
+print("\n")
+### Expressions Part 3
+## A Word frequency script
+# Get the name of the file and open it
+#name = input('Enter file:')
+#handle = open(name, 'r')
+
+# Count word frequency
+#counts = dict()
+#for line in handle:
+#    words = line.split()
+#    for word in words:
+#        count[word] = counts.get(word,0) + 1
+
+# Find the most common word
+#bigcount = None
+#bigword = None
+#for word,count in counts.items():
+#    if bigcount is None or count > bigcount:
+#        bigword = word
+#        bigcount = count
+
+# All done
+#print(bigword, bigcount)
+
+## Converting User Input
+# If we want to read a number from the user, we must
+# convert it from a string to a number using a type
+# conversion function
+
+# Later we will deal with bad input data
+
+# Example 1 - Asks for the EU floor and adds 1
+# Convert elevator floors
+#inp = input('Europe floor?')
+#usf = int(inp) + 1 # converts string to int
+#print('US Floor', usf)
+
+print("----------------------------------\n")
+print("The end of the line: nothing is here")
