1640d1d1e23c9005b15e133b40621d07a916c681
Nash Hymet <nashhymet@gmail.com>
2019-11-20 17:18:23 -0600

    Finished week 06

---
+++ MyProgress/First Year/Semester 1/Python for Everyone/Week 06/4_1_notes.py
@@ -0,0 +1,19 @@
+print("Output:")
+print("----------------------------------")
+print("\n")
+### Using Functions
+## Store and reuse
+#def thing():
+#    print('Hello World')
+#    print('Fun times ahead')
+
+#thing()
+#print('Zip')
+#thing()
+
+# max() determines w bigger
+#big = max('Hello world')
+#print(big)
+
+print("----------------------------------\n")
+print("The end of the line: nothing is here")
---
+++ MyProgress/First Year/Semester 1/Python for Everyone/Week 06/4_2_notes.py
@@ -0,0 +1,23 @@
+print("Output:")
+print("----------------------------------")
+print("\n")
+### Building Functions
+## Use def to create the function name
+#def myCoolFunction(myInt):
+#    if myInt == 2:
+#        print('Hello World! I am Firit.')
+#    elif myInt == 4:
+#        print('Double Firit Mode!')
+#    else:
+#        print ('Normal Firit Mode')
+
+#myCoolFunction(4)
+
+# function to return based on print inputted
+#def greet():
+#    return "Hello"
+#
+#print(greet(), "Glenn")
+
+print("----------------------------------\n")
+print("The end of the line: nothing is here")
